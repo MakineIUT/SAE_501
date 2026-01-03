8101a9493f1e1d6816963f226c66fc4fc5b68779
unknown <hounsoumarkhus2004@gmail.com>
2026-01-03 20:04:35 +0100

    correctif

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 
 // Mettre à false pour utiliser la vraie API (quand elle sera prête)
 // Mettre à true pour utiliser le mode test (localStorage local sans backend)
-export const USE_MOCK_AUTH = false
+export const USE_MOCK_AUTH = true
